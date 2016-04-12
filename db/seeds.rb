af50dce6c96089b08b5999a704a30e6cfc30c3d4
Josh Nichols <josh.nichols.lives@gmail.com>
2016-04-12 16:32:57 -0600

    seed database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+users = User.create([{
+     username: 'admin', 
+     password: 'jO123lly'
+     role: 'admin'
+  }])
