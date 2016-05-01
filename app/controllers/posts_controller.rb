6d05adbdfc8743286e15442c0aabce6ac52660d4
Josh Nichols <josh.nichols.lives@gmail.com>
2016-04-30 21:52:10 -0600

    blog search functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,11 @@ class PostsController < ApplicationController
   before_action :require_admin, only: [:new, :create, :edit, :update, :destroy]
   def index
     @posts = Post.all
+    if params[:search]
+     @posts = Post.search(params[:search]).order("created_at DESC")
+   else
+     @posts = Post.all.order('created_at DESC')
+   end
   end
   def show
     @post = Post.find(params[:id])
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,6 @@
 class Post < ActiveRecord::Base
   has_many :comments, dependent: :destroy
+  def self.search(search)
+   where("title LIKE ? OR content LIKE?", "%#{search}%", "%#{search}%")
+  end
 end
